6903eba7058ba1d9f2e185a119019ef62146a4b1
IliyaBrook <iliyabrook1987@gmail.com>
2022-06-26 23:35:07 +0200

    start

--- routes/webhooks/statusChange.js
+++ routes/webhooks/statusChange.js
@@ -17,15 +17,19 @@ router.post('/changeStatus',  (req, res, next) => {
 
 
     console.log('brook board: ', boardId)
-    console.log('brook itemId: ', itemId)
     console.log('brook groupId: ', groupId)
+    console.log('brook itemId: ', itemId)
 
-
-    next()
+    fetch(queryItems(boardId, groupId, itemId))
+        .then(queryRes => {
+            console.log('query response:', queryRes)
+        }).catch(queryError => console.log('queryError: ', queryError))
 
     //Register monday webhook
     app.post("/", function(req, res) { console.log(JSON.stringify(req.body, 0, 2)); res.status(200).send(req.body);})
 });
+
+
 
 
 module.exports = router;
