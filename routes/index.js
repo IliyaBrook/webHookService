1b1c8243c5f1573163aa46e40001dcf979be60b0
IliyaBrook <iliyabrook1987@gmail.com>
2022-06-26 18:46:50 +0200

    start

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const app = require('../app');
 
 /* GET home page. */
-app.post('/', async (req, res, next) => {
-  console.log('Monday request:', JSON.stringify(req.body, null , 2))
-  res.status(200).send(req.body);
+app.post('/', function (req, res) {
+    console.log(JSON.stringify(req.body, null, 2));
+    res.status(200).send(req.body);
 });
+
 
 module.exports = router;
 
