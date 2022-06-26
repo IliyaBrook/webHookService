a0fca2f5944635d1eac3ed1d3602a6dcfde58890
IliyaBrook <iliyabrook1987@gmail.com>
2022-06-27 01:14:58 +0200

    start

--- routes/webhooks/statusChange.js
+++ routes/webhooks/statusChange.js
@@ -21,13 +21,53 @@ router.post('/changeStatus',  (req, res, next) => {
             console.log('query response:', JSON.stringify(data, null, 2))
             const itemData = data.data.boards[0].groups[0].items[0]
             console.log('brook item data:', itemData);
+
             const itemName = itemData.name;
-            console.log('brook item data name xx:', itemName);
+            let salesman = {};
+
+            let saleAmount, dateOfSigning,
+                leadDate, address,
+                phone, email, leadComeFrom,
+                leadNumber, courseId;
+
+            itemData.column_values.forEach(column => {
+                switch (column.id) {
+                    case 'person':
+                        return salesman = column;
+                    case 'numbers':
+                        return saleAmount = column.text;
+                    case '___________________8':
+                        return dateOfSigning = column.text;
+                    case 'date1':
+                        return leadDate = column.text
+                    case 'location6':
+                        return address = column.text;
+                    case 'text':
+                        return phone = column.text;
+                    case 'text6':
+                        return email = column.email;
+                    case 'text64':
+                        return leadComeFrom = column.text;
+                    case "text01":
+                        return leadNumber = column.text;
+                    case "connect_boards":
+                       return courseId = JSON.parse(column.value).linkedPulseIds[0].linkedPulseId;
+                }
+            })
+
+            console.log("brook saleAmount:",saleAmount)
+            console.log("brook dateOfSigning:",dateOfSigning)
+            console.log("brook leadDate:",leadDate)
+            console.log("brook address:",address)
+            console.log("brook phone:",phone)
+            console.log("brook email:",email)
+            console.log("brook leadComeFrom:",leadComeFrom)
+            console.log("brook leadNumber:",leadNumber)
+            console.log("brook courseId:",courseId)
+
         }).catch(queryError => console.log('queryError: ', queryError))
 
     res.post(res.status(200).send(req.body))
-    //Register monday webhook
-    // app.post("/", function(req, res) { console.log(JSON.stringify(req.body, 0, 2)); res.status(200).send(req.body);})
 });
 
 
