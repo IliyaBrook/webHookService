aa96ec1c0a41cc99794bca2d8faceb9edeaa4dbd
IliyaBrook <iliyabrook1987@gmail.com>
2022-06-28 00:58:02 +0200

    start

--- querys/createSubitem.js
+++ querys/createSubitem.js
@@ -1,22 +1,23 @@
 module.exports = ( name, parentId, object ) => {
     console.log('brook provided obj:', object)
-    console.log('brook provided name:', object)
+    console.log('brook provided name:', name)
     console.log('brook provided parentId:', parentId)
+    const date = '28/06/2022'
     return `
      mutation {
       create_subitem(
       parent_item_id: ${parentId}, 
       item_name: ${name}, 
       column_values: \"{
-      \\\"owner\\\":\\\"${object?.owner || 'no value'}\\\",
-      \\\"date\\\":\\\"${object?.date || 'no value'}\\\",
-      \\\"text_14\\\":\\\"${object?.leadId  || 'no value'}\\\",
-      \\\"text\\\":\\\"${object?.leadComeFrom  || 'no value'}\\\",
-      \\\"text4\\\":\\\"${object?.address  || 'no value'}\\\",
-      \\\"text_17\\\":\\\"${object?.phone  || 'no value'}\\\",
-      \\\"text_2\\\":\\\"${object?.email  || 'no value'}\\\",
-      \\\"text_196\\\":\\\"${object?.salesMan  || 'no value'}\\\",
-      \\\"______\\\":\\\"${object?.saleAmount  || 'no value'}\\\"
+      \\\"owner\\\":\\\"${object.owner}\\\",
+      \\\"date\\\":\\\"${date}\\\",
+      \\\"text_14\\\":\\\"${object.leadId}\\\",
+      \\\"text\\\":\\\"${object.leadComeFrom}\\\",
+      \\\"text4\\\":\\\"${object.address}\\\",
+      \\\"text_17\\\":\\\"${object.phone}\\\",
+      \\\"text_2\\\":\\\"${object.email}\\\",
+      \\\"text_196\\\":\\\"${object.salesMan}\\\",
+      \\\"______\\\":\\\"${object.saleAmount}\\\"
       }"
         ){
           column_values{
