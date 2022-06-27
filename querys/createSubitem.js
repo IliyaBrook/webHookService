b9670226bea19d2fcbad9bdb9dafe7e3a0e5417e
IliyaBrook <iliyabrook1987@gmail.com>
2022-06-28 01:02:53 +0200

    start

--- querys/createSubitem.js
+++ querys/createSubitem.js
@@ -2,23 +2,11 @@ module.exports = ( name, parentId, object ) => {
     console.log('brook provided obj:', object)
     console.log('brook provided name:', name)
     console.log('brook provided parentId:', parentId)
-    const date = '28/06/2022'
     return `
      mutation {
       create_subitem(
       parent_item_id: ${parentId}, 
-      item_name: ${name}, 
-      column_values: \"{
-      \\\"owner\\\":\\\"${object.owner}\\\",
-      \\\"date\\\":\\\"${date}\\\",
-      \\\"text_14\\\":\\\"${object.leadId}\\\",
-      \\\"text\\\":\\\"${object.leadComeFrom}\\\",
-      \\\"text4\\\":\\\"${object.address}\\\",
-      \\\"text_17\\\":\\\"${object.phone}\\\",
-      \\\"text_2\\\":\\\"${object.email}\\\",
-      \\\"text_196\\\":\\\"${object.salesMan}\\\",
-      \\\"______\\\":\\\"${object.saleAmount}\\\"
-      }"
+      item_name: ${name}
         ){
           column_values{
           id
