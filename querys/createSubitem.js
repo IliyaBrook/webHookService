7d1e1ba5d1a9f052d92bc4554781e9a8019e372e
IliyaBrook <iliyabrook1987@gmail.com>
2022-06-28 01:15:49 +0200

    start

--- querys/createSubitem.js
+++ querys/createSubitem.js
@@ -6,7 +6,18 @@ module.exports = ( name, parentId, object ) => {
      mutation {
       create_subitem(
       parent_item_id: ${parentId}, 
-      item_name: ${name}
+      item_name: ${name}, 
+      column_values: \"{
+      \\\"owner\\\":\\\"${object?.owner || 'no owner value'}\\\",
+      \\\"date\\\":\\\"${object?.date || 'no date value'}\\\",
+      \\\"text_14\\\":\\\"${object?.leadId || 'no leadId value'}\\\",
+      \\\"text\\\":\\\"${object?.leadComeFrom || 'no value found'}\\\",
+      \\\"text4\\\":\\\"${object?.address || 'no address value'}\\\",
+      \\\"text_17\\\":\\\"${object?.phone || 'no phone value'}\\\",
+      \\\"text_2\\\":\\\"${object?.email || 'no email value'}\\\",
+      \\\"text_196\\\":\\\"${object?.salesMan || 'no sales man value'}\\\",
+      \\\"______\\\":\\\"${object?.saleAmount || 'no sale amount value'}\\\"
+      }"
         ){
           column_values{
           id
