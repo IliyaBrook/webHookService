5874e7d1579668e561ad2742918ea03be8efd6af
IliyaBrook <iliyabrook1987@gmail.com>
2022-06-27 18:00:57 +0200

    start

--- querys/createSubitem.js
+++ querys/createSubitem.js
@@ -8,7 +8,7 @@ module.exports = ( name, parentId, object ) => `
       \\\"text_2\\\":\\\"${object.email}\\\",
       \\\"text_14\\\":\\\"${object.leadId}\\\",
       \\\"text4\\\":\\\"${object.address}\\\",
-      \\\""text"\\\":\\\"mpg.org.il\\\"
+      \\\"text\\\":\\\"mpg.org.il\\\"
       }"
         ){
           column_values{
