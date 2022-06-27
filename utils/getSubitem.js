40499e491b134b14f30c8d59ac18e8675a441cdb
IliyaBrook <iliyabrook1987@gmail.com>
2022-06-27 14:07:54 +0200

    start

--- utils/getSubitem.js
+++ utils/getSubitem.js
@@ -14,11 +14,11 @@ module.exports = async (itemId) => {
     const data2 = await useFetch(queryColumnValues(itemId))
     console.log('brook response data 2222:', data2)
 
-    const columnValues = data.data?.boards[0].items.column_values;
-
-    console.log('brook columnValues:', columnValues)
-    const subItemValue = columnValues[0].filter(value => value.type === "subtasks")
-    const valueProd = JSON.parse(subItemValue[0].value ).linkedPulseIds[0].linkedPulseId;
-    const valueDev = JSON.parse(subItemValue[1].value ).linkedPulseIds[0].linkedPulseId;
-    return {valueProd, valueDev}
+    // const columnValues = data.data?.boards[0].items.column_values;
+    //
+    // console.log('brook columnValues:', columnValues)
+    // const subItemValue = columnValues[0].filter(value => value.type === "subtasks")
+    // const valueProd = JSON.parse(subItemValue[0].value ).linkedPulseIds[0].linkedPulseId;
+    // const valueDev = JSON.parse(subItemValue[1].value ).linkedPulseIds[0].linkedPulseId;
+    // return {valueProd, valueDev}
 }
