17b0e1c3da2bb742a9bd13f4a11ff31d72bef6d8
IliyaBrook <iliyabrook1987@gmail.com>
2022-06-27 13:30:40 +0200

    start

--- utils/getSubitem.js
+++ utils/getSubitem.js
@@ -4,6 +4,9 @@ const queryColumnValues = require('../querys/getItemColumnValues');
 module.exports = async (itemId) => {
     const response = await useFetch(queryColumnValues(itemId))
     const columnValues = response.data?.boards[0].items[0].column_values;
+    console.log('brook boards:', response.data?.boards)
+    console.log('brook columnValues:', columnValues)
+
     const subItemValue = columnValues.filter(value => value.type === "subtasks")
     const valueProd = JSON.parse(subItemValue[0].value ).linkedPulseIds[0].linkedPulseId;
     const valueDev = JSON.parse(subItemValue[1].value ).linkedPulseIds[0].linkedPulseId;
